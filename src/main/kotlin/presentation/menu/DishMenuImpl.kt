1cbc607340a1e38d60fc0bd660607b804d8a721d
Anzhelika Gakal <asgakal@edu.hse.ru>
2024-03-03 05:07:12 +0300

    implement admin functions

--- src/main/kotlin/presentation/menu/DishMenuImpl.kt
+++ src/main/kotlin/presentation/menu/DishMenuImpl.kt
@@ -8,41 +8,42 @@ import services.interfaces.MenuService
 import services.models.AdminResponse
 
 class DishMenuImpl(private val menuService: MenuService) : DishMenu {
-
+    private val hintNotEmptyString = "Name should be not empty\n\n"
     private fun setDifficulty(): AdminResponse {
-        TODO("Not yet implemented")
+        val name = getAndValidateDishName() ?:
+            return AdminResponse(404, hintNotEmptyString, null)
+        val difficulty = getValidatedDifficulty() ?:
+            return AdminResponse(404, "Not valid duration\n\n", null)
+        val response = menuService.setDifficultyOfDish(name, difficulty)
+        return response
     }
 
     private fun setPrice(): AdminResponse {
-        TODO("Not yet implemented")
+        val name = getAndValidateDishName() ?:
+            return AdminResponse(404, hintNotEmptyString, null)
+        val price = getValidatedPrice() ?:
+            return AdminResponse(404, "Not valid price\n\n", null)
+        val response = menuService.setPriceOfDish(name, price)
+        return response
     }
 
+
     private fun setAmount(): AdminResponse {
-        TODO("Not yet implemented")
+        val name = getAndValidateDishName() ?:
+            return AdminResponse(404, hintNotEmptyString, null)
+        val amount = getValidatedAmount() ?:
+            return AdminResponse(404, "Not valid amount\n\n", null)
+        val response = menuService.setAmountOfDish(name, amount)
+        return response
     }
 
     private fun dropDish(): AdminResponse {
-        TODO("Not yet implemented")
+        val name = getAndValidateDishName() ?:
+        return AdminResponse(404, hintNotEmptyString, null)
+        val response = menuService.dropDish(name)
+        return response
     }
 
-    fun createAndValidateDishParameters() : AdminResponse {
-        println("Enter name of dish: ")
-        val name: String = readlnOrNull() ?: return AdminResponse(404, "Name should be not empty\n\n", null)
-        println("Enter amount of dish: ")
-        val amount: Int =
-            readlnOrNull()?.toIntOrNull() ?: return AdminResponse(404, "Amount should be not empty\n\n", null)
-        println("Enter price of dish: ")
-        val price: Double = readlnOrNull()?.toDoubleOrNull() ?: return AdminResponse(
-            404,
-            "Price should be not empty\n\n",
-            null
-        )
-        println("Enter duration of preparing in minutes of dish: ")
-        val duration: Int =
-            readlnOrNull()?.toIntOrNull() ?: return AdminResponse(404, "Duration should be not empty\n\n", null)
-        val newDish = Dish(name, amount, price, duration)
-        return AdminResponse(200, "Dish was successfully created\n\n", newDish)
-    }
     private fun addOrReplaceDish(): AdminResponse {
         var response = createAndValidateDishParameters()
         val dish = response.dish
@@ -59,6 +60,51 @@ class DishMenuImpl(private val menuService: MenuService) : DishMenu {
             else -> AdminResponse(404, "Incorrect input\n\n", null)
         }
         return response
+    }
+
+    private fun getValidatedPrice(): Double? {
+        println("Enter price of dish: ")
+        val price: Double? = readlnOrNull()?.toDoubleOrNull()
+        if(price == null || price < 0)
+            return null
+        return price
+    }
+
+    private fun getValidatedDifficulty(): Int? {
+        println("Enter duration of preparing in minutes of dish: ")
+        val duration: Int? = readlnOrNull()?.toIntOrNull()
+        if(duration == null || duration < 0)
+            return null
+        return duration
+    }
+
+    private fun getAndValidateDishName() : String? {
+        println("Enter name of dish: ")
+        val name: String? = readlnOrNull()
+        if(name == "" || name == null)
+            return null
+        return name
+    }
+
+    private fun getValidatedAmount(): Int? {
+        println("Enter amount of dish: ")
+        val amount: Int? = readlnOrNull()?.toIntOrNull()
+        if(amount == null || amount < 0)
+            return null
+        return amount
+    }
+
+    private fun createAndValidateDishParameters() : AdminResponse {
+        val name = getAndValidateDishName() ?:
+            return AdminResponse(404, hintNotEmptyString, null)
+        val amount = getValidatedAmount() ?:
+            return AdminResponse(404, "Not valid amount\n\n", null)
+        val price = getValidatedPrice() ?:
+            return AdminResponse(404, "Not valid price\n\n", null)
+        val difficulty = getValidatedDifficulty() ?:
+            return AdminResponse(404, "Not valid duration\n\n", null)
+        val newDish = Dish(name, amount, price, difficulty)
+        return AdminResponse(200, "Dish was successfully created\n\n", newDish)
     }
 
     private fun exit(): AdminResponse {
--- src/main/kotlin/presentation/models/AllMenuPresentations.kt
+++ src/main/kotlin/presentation/models/AllMenuPresentations.kt
@@ -11,6 +11,7 @@ class AllMenuPresentations {
             "2. Drop dish from the menu,\n" +
             "3. Set amount for dish,\n" +
             "4. Set price for dish,\n" +
-            "5. Set difficulty for dish\n"
+            "5. Set difficulty for dish\n" +
+            "6. Exit\n"
 
 }
